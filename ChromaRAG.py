c9632a7bd659656b2373081987e257b484b3f29b
Alex <alex.direnza@uconn.edu>
2025-04-03 13:55:25 -0400

    Added template files and location generation
    - Removed think from deepseek model responses
    - Added ability to read from template files for prompts
    - Added `generate_location()` method

--- ChromaRAG.py
+++ ChromaRAG.py
@@ -2,12 +2,14 @@ import chromadb
 import os
 import ollama
 import json
+from html.parser import HTMLParser
 
 class ChromaRAG():
-    def __init__(self, doc_path="./docs", collection_path="./collection", model_name="deepseek-r1:7b"):
+    def __init__(self, doc_path="./docs", collection_path="./collection", template_path="./templates", model_name="deepseek-r1:7b"):
         self.doc_path = doc_path
         self.model_name = model_name
         self.collection_path = collection_path
+        self.template_path = template_path
 
         self.chroma = chromadb.PersistentClient(path=collection_path)
         
@@ -69,36 +71,11 @@ class ChromaRAG():
                 returns the output from the model
         """
 
-        prompt = """###Task: 
-
-    Respond to the user query using the provided context, incorporating inline citations in the format [name_of_source] where name_of_source is the title of the source the citation is from. 
-
-###Guidelines: 
-
-    If you don't know the answer, clearly state that. 
-
-    If uncertain, ask the user for clarification. 
-
-    If the answer isn't present in the context but you possess the knowledge, explain this to the user and provide the answer using your own understanding. 
-
-    Cite sources using the filename before the context
-
-    Ensure citations are concise and directly related to the information provided. 
-
-###Example of Citation: 
-
-    If the user asks about a specific topic and the information is found in \"whitepaper.pdf\" with a provided , the response should include the citation like so:  
-
-    \"According to the study, the proposed method increases efficiency by 20% [whitepaper.pdf].\" If no context is present, the response should omit the citation. 
-"""
-
         collection_results = self.collection.query(query_texts=[in_prompt], n_results = 3)
         
         context = context_to_string(collection_results["documents"][0], collection_results["ids"][0])
 
-        if context:
-            prompt += "###Context\n\n" + context
-        prompt += "###Query\n\n" + in_prompt
+        prompt = self.format_prompt(in_prompt, context, "query")
 
         response = ollama.generate(model=MODEL_NAME, prompt=prompt)['response'].split("</think>")
         if len(response) > 1:
@@ -119,22 +96,22 @@ class ChromaRAG():
                 additional_info
         """
         prompt = "Generate a character "
-        if prompt_dict['person_name']:
+        if prompt_dict['person_name'] != "":
             prompt += "named " + prompt_dict['person_name'] + " "
 
-        if prompt_dict['person_home']:
+        if prompt_dict['person_home'] != "":
             prompt += "from " + prompt_dict['person_home'] + " "
 
-        if prompt_dict['profession']:
+        if prompt_dict['profession'] != "":
             prompt += "who is a " + prompt_dict['profession'] + " "
 
-        if prompt_dict['faction']:
+        if prompt_dict['faction'] != "":
             prompt += "and is a part of " + prompt_dict['faction'] + " "
 
-        if prompt_dict['relationships']:
+        if prompt_dict['relationships'] != "":
             prompt += "\nIn addition the character knows of:\n" + prompt_dict['relationships']
 
-        if prompt_dict['additional_info']:
+        if prompt_dict['additional_info'] != "":
             prompt += "\nThe character also:\n" + prompt_dict['additional_info']
 
         
@@ -145,11 +122,70 @@ class ChromaRAG():
 
         context = "###Context\n\n" + context
         
-        prompt = format_character_prompt(prompt, context)
+        prompt = self.format_prompt(prompt, context, "character")
+
+        response = ollama.generate(self.model_name, prompt)['response'].split("</think>")
+
+        if len(response) > 1:
+            return response[1]
+        else:
+            return response[0]
+
+
+
+    def generate_location(self, prompt_dict):
+        """Dictionary contains:
+                place_name
+                nearby
+                faction
+                location_type
+                sub_locations
+                additional_info
+        """
+        prompt = "Generate a place "
+        if prompt_dict['place_name'] != "":
+            prompt += "named " + prompt_dict['place_name'] + " "
+
+        if prompt_dict['nearby'] != "":
+            prompt += "near " + prompt_dict['nearby'] + " "
+
+        if prompt_dict['location_type'] != "":
+            prompt += "which is a " + prompt_dict['location_type'] + " "
+
+        if prompt_dict['faction'] != "":
+            prompt += "ruled by " + prompt_dict['faction'] + " "
+
+        if prompt_dict['sub_locations'] != "":
+            prompt += "\nIn addition within this place there are:\n" + prompt_dict['sub_locations']
+
+        if prompt_dict['additional_info'] != "":
+            prompt += "\nThe place also:\n" + prompt_dict['additional_info']
 
         
 
-        return ollama.generate(self.model_name, prompt)['response']
+        collection_results = self.collection.query(query_texts=[prompt], n_results = 3)
+        
+        context = context_to_string(collection_results["documents"][0], collection_results["ids"][0])
+
+        context = "###Context\n\n" + context
+        
+        prompt = self.format_prompt(prompt, context, "location")
+
+        response = ollama.generate(self.model_name, prompt)['response'].split("</think>")
+
+        if len(response) > 1:
+            return response[1]
+        else:
+            return response[0]
+
+
+
+    def format_prompt(self, in_prompt, context, template):
+        template = open(self.template_path + "/" + template + ".template").read()
+        return template + context + "###Query\n\n" + in_prompt
+
+
+
 
 class html_ripper(HTMLParser):
     def __init__(self):
@@ -169,51 +205,6 @@ def context_to_string(documents, ids):
         res += documents[x]
         res += "\"\"\"\n\n"
     return res
-
-def format_character_prompt(in_prompt, context):
-    return """###Task:
-
-    Generate a character for a fantasy world described by the given context under the conditions provided by the user in query with an output formatted the same way as the Template
-
-###Guidelines: 
-
-    Connect the generated content to the given context if possible
-
-    Keep content to a generic fantasy style
-
-###Template:
-
-\"\"\"
-Name
----
-[Name of the character]
-
-Description
----
-[Brief overview of the character]
-
-Skills
----
-[Things the character is good or bad at]
-
-Personality
----
-[The personality of the character]
-
-Backstory
----
-[A backstory of where the character comes from]
-
-Relationships
----
-[Relationships with other characters and factions in the world]
-
-\"\"\"
-
-###Query
-
-""" + context + in_prompt 
-
 
 MODEL_NAME = "deepseek-r1:7b"
 TEMPLATE_PATH = "./templates"
